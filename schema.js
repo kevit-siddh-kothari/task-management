c65660e60a9c389d5a55c05dc64d32feb8582ff8
siddh <siddh.kothari@kevit.io>
2024-07-05 15:30:05 +0530

    Updated

---
+++ schema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+
+
+//schema defined
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    jobTitle: {
+        type: String,
+    },
+},{timestamps:true})
+
+//creating a model
+const user = mongoose.model('user',userSchema)
+
+module.exports ={
+    user
+}
