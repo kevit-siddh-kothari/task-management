d45323c936c19e22294ed2256fa46a9d45dc3863
siddh <siddh.kothari@kevit.io>
2024-07-22 12:15:05 +0530

    bcrypt library added

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,18 +1,22 @@
 const {Auth} = require('../models/auth')
+const bcrypt = require('bcrypt')
 
 const SignIn = async(req,res)=>{
     const {username,password} = req.body
+    const hashedPassword = await bcrypt.hash(password,8)
     console.log(username,password)
     const auth = await Auth.create({
         username:username,
-        password:password
+        password:hashedPassword
     })
     await auth.save()
     res.send('signed in sucessfully')
 }
 const LogIn = async(req,res)=>{
     const {username,password} = req.params
-    const check = await Auth.findOne({username:username, password:password})
+    const user = await Auth.findOne({username:username})
+    console.log(user)
+    const check = await bcrypt.compare(password,user.password)
     if(check){
         res.redirect('/api/user/')
     }else{
